3c3a4c6a54d6006e65e2899d57f9f4b2e26ba658
md-nasim-mondal <nasim.programmer24.com>
2024-09-28 02:00:04 +0600

    made dashboard page upper part

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,83 +1,136 @@
-"use client"
-import ProjectCard from '@/components/Dashboard/ProjectCard';
-import Tabs from '@/components/Dashboard/Tabs';
-import React, { useState } from 'react';
+"use client";
+import ProjectCard from "@/components/Dashboard/ProjectCard";
+import Tabs from "@/components/Dashboard/Tabs";
+import Image from "next/image";
+import React, { useState } from "react";
 
 const Page = () => {
-  const [filter, setFilter] = useState('Ongoing');
-  const filteredProjects = projects.filter((project) => project.status === filter);
+  const [filter, setFilter] = useState("Ongoing");
+  const filteredProjects = projects.filter(
+    (project) => project.status === filter
+  );
 
   return (
-    <div className="container mx-auto p-8">
-      <Tabs setFilter={setFilter} />
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mt-4 justify-around">
-        {filteredProjects?.map((project) => (
-          <ProjectCard key={project.id} project={project} />
+    <div className='container mx-auto py-8 p-6'>
+      <div className='space-y-2'>
+        <h2 className='font-bold text-3xl text-[#050504]'>
+          Welcome Back, Haseena!
+        </h2>
+        <p className='text-[#707066]'>
+          You have accomplished a lot today. Let us handle the rest.
+        </p>
+      </div>
+      <div className='flex gap-12 my-6'>
+        {statuses?.map(({ id, img, title, totalNumber}) => (
+          <div key={id} className='flex gap-2'>
+            <Image src={img} alt='Folder-image' width={40} height={5} />
+            <p className='flex flex-col'>
+              <span className='font-bold text-[#050504]'>{totalNumber}</span>
+              <span className='text-[#707066]'>{title}</span>
+            </p>
+          </div>
         ))}
       </div>
       <div>
-        {
-          !filteredProjects?.length && <>
-            <div>
-              <h3 className="text-3xl mt-12 font-semibold">You Have No {filter} Projects.</h3>
-          </div>
-          </>
-        }
+        <Tabs setFilter={setFilter} />
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mt-4 justify-around'>
+          {filteredProjects?.map((project) => (
+            <ProjectCard key={project.id} project={project} />
+          ))}
+        </div>
+        <div>
+          {!filteredProjects?.length && (
+            <>
+              <div>
+                <h3 className='text-3xl mt-12 font-semibold'>
+                  You Have No {filter} Projects.
+                </h3>
+              </div>
+            </>
+          )}
+        </div>
       </div>
     </div>
   );
 };
 
-const projects = [
+const statuses = [
   {
     id: 1,
-    title: 'Make an E-Commerce Website for a Brand Store',
-    category: 'Web',
-    milestone: 'Dec 5th',
-    status: 'Ongoing',
+    title: "Completed",
+    img: "/assets/images/Folder.png",
+    totalNumber: 30,
   },
   {
     id: 2,
-    title: 'Website technical maintenance project',
-    category: 'Web',
-    milestone: 'Jan 1st',
-    status: 'Ongoing',
+    title: "Ongoing",
+    img: "/assets/images/Folder(1).png",
+    totalNumber: "02",
   },
   {
     id: 3,
-    title: 'Website technical maintenance project',
-    category: 'Web',
-    milestone: 'In 3 days',
-    status: 'Ongoing',
+    title: "Cancelled",
+    img: "/assets/images/Folder(2).png",
+    totalNumber: "04",
   },
   {
     id: 4,
-    title: 'Website technical maintenance project',
-    category: 'Web',
-    milestone: 'In 3 days',
-    status: 'Ongoing',
+    title: "Drafts",
+    img: "/assets/images/Folder(3).png",
+    totalNumber: "02",
+  },
+];
+
+const projects = [
+  {
+    id: 1,
+    title: "Make an E-Commerce Website for a Brand Store",
+    category: "Web",
+    milestone: "Dec 5th",
+    status: "Ongoing",
+  },
+  {
+    id: 2,
+    title: "Website technical maintenance project",
+    category: "Web",
+    milestone: "Jan 1st",
+    status: "Ongoing",
+  },
+  {
+    id: 3,
+    title: "Website technical maintenance project",
+    category: "Web",
+    milestone: "In 3 days",
+    status: "Ongoing",
+  },
+  {
+    id: 4,
+    title: "Website technical maintenance project",
+    category: "Web",
+    milestone: "In 3 days",
+    status: "Ongoing",
   },
   {
     id: 5,
-    title: 'Make an E-Commerce Website for a Brand Store',
-    category: 'Web',
-    milestone: 'Dec 5th',
-    status: 'Ongoing',
+    title: "Make an E-Commerce Website for a Brand Store",
+    category: "Web",
+    milestone: "Dec 5th",
+    status: "Ongoing",
   },
   {
     id: 6,
-    title: 'Website technical maintenance project',
-    category: 'Web',
-    milestone: 'Jan 1st',
-    status: 'Ongoing',
+    title: "Website technical maintenance project",
+    category: "Web",
+    milestone: "Jan 1st",
+    status: "Ongoing",
   },
   {
     id: 7,
-    title: 'Website technical maintenance project',
-    category: 'Web',
-    milestone: 'In 3 days',
-    status: 'Ongoing',
+    title: "Website technical maintenance project",
+    category: "Web",
+    milestone: "In 3 days",
+    status: "Ongoing",
   },
-]
+];
 
 export default Page;
--- src/components/Dashboard/ProjectCard.jsx
+++ src/components/Dashboard/ProjectCard.jsx
@@ -4,13 +4,13 @@ import React from "react";
 const ProjectCard = ({project}) => {
   const { title, category, milestone, status } = project || {};
   return (
-    <div className='rounded-md shadow-md p-6'>
+    <div className='rounded-md shadow-md p-4'>
       {/* Header: Title and Menu Icon */}
       <div>
         <div className='flex justify-between'>
           <div>
-            <h3 className="text-[#050504] font-bold text-xl">{title}</h3>
-            <p className="text-xs text-[#707066] pt-3">{category}</p>
+            <h3 className="text-[#050504] font-bold text-lg">{title}</h3>
+            <p className="text-xs text-[#707066] pt-2">{category}</p>
           </div>
           <span>
             <Image src={'/assets/icons/IconDots.svg'} alt="More-btn-Icon" height={50} width={50} />
@@ -18,7 +18,7 @@ const ProjectCard = ({project}) => {
         </div>
 
         {/* Custom Progress Bar */}
-        <div className='my-4 flex justify-between items-center'>
+        <div className='my-2 flex justify-between items-center'>
           <div className='flex items-center justify-center'>
             {/* Circles for each step */}
             <div style={stepStyle(true)}></div>
@@ -35,14 +35,14 @@ const ProjectCard = ({project}) => {
         </div>
 
         {/* Status Tags */}
-        <div className='space-y-2'>
-          <span className="bg-[#DBE2F0] py-1 px-2 rounded-md">{status}</span> <br/> <br />
-          <span className="bg-[#DBE2F0] py-1 px-2 rounded-md">Next milestone: {milestone}</span>
+        <div className='flex flex-col gap-1'>
+          <span className="bg-[#DBE2F0] py-1 px-2 rounded-md w-fit">{status}</span> 
+          <span className="bg-[#DBE2F0] py-1 px-2 rounded-md w-fit">Next milestone: {milestone}</span>
         </div>
       </div>
 
       {/* Action Section */}
-      <div className='flex justify-between items-center mt-4'>
+      <div className='flex justify-between items-center mt-2'>
         <h4 className="text-[#050504] font-bold mt-auto">Follow up with manager</h4>
         <button className="bg-[#5876b7] p-2 rounded-md">
           <Image src={'/assets/icons/Send.svg'} alt="Send-Icon" width={20} height={20} />
@@ -63,13 +63,13 @@ const stepStyle = (completed) => ({
 
 const lineStyle = {
   height: 3,
-  width: 50,
+  width: 60,
   backgroundColor: "#5876B7", // Blue connecting lines
   display: "inline-block",
 };
 const lineStyle1 = {
   height: 3,
-  width: 50,
+  width: 60,
   backgroundColor: "#DADAD7", // Blue connecting lines
   display: "inline-block",
 };
